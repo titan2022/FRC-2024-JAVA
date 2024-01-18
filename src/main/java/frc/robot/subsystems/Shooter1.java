a053283b91b7231c0745329da5f6b837ea6d9259
Aryan <aryan.mansingh15@gmail.com>
2024-01-18 09:25:14 -0600

    Start to Shooter1

--- src/main/java/frc/robot/subsystems/Shooter1.java
+++ src/main/java/frc/robot/subsystems/Shooter1.java
@@ -1,5 +1,38 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.CANCoderStatusFrame;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+
+
 public class Shooter1 {
-    
+    public static final int portnumber1 = 1;
+    public static final int portnumber2 = 2;
+    //public static final portnumber3 = insert port number
+    //public static final portnumber4 = insert port number
+
+    public static final int 
+
+    private final WPI_TalonFX[] motors = new WPI_TalonFX[] {
+        new WPI_TalonFX(portnumber1),
+        new WPI_TalonFX(portnumber2),
+        //new WPI_TalonFX(portnumber3),
+        //new WPI_TalonFX(portnumber4)
+    };
+
+    public Shooter1(){
+
+    }
+
+    public setVelocity(int velocityPercent){
+        
+    }
+
 }
