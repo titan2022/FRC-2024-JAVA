6103abbfc9d8e9d5ee5a19916ac93f94a8b324e4
Ivan Chen <ivanchen3k@gmail.com>
2024-01-18 10:50:06 -0600

    Wrote RotationCommand

--- src/main/java/frc/robot/commands/RotationCommand.java
+++ src/main/java/frc/robot/commands/RotationCommand.java
@@ -4,35 +4,58 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.RotationalDrivebase;
 
 /** An example command that uses an example subsystem. */
 public class RotationCommand extends Command {
+  private RotationalDrivebase driveBase;
+  private Rotation2d angle;
+  private double speed;
+  private double time;
+  private double startTime;
+  private double endTime;
+
   @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
   public RotationCommand(double angle, double speed, RotationalDrivebase driveBase) {
+    this.driveBase = driveBase;
+    this.angle = Rotation2d.fromDegrees(angle);
+    this.speed = speed;
 
+    time = this.angle.getDegrees() / speed;
+
+    addRequirements(driveBase);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    startTime = Timer.getFPGATimestamp();
+    endTime = startTime + time;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    driveBase.setRotation(angle.getRadians());
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    driveBase.setRotation(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    if (Timer.getFPGATimestamp() < endTime) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
