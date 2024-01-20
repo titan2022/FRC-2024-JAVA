bc8ad5954d2686016d518980d5e08a6f3f74a68f
nekiwo <58634064+nekiwo@users.noreply.github.com>
2024-01-19 23:58:57 -0600

    type interfaces for networking server

---
+++ src/main/java/frc/robot/utility/networking/types/NetworkingTag.java
@@ -0,0 +1,18 @@
+package frc.robot.utility.networking.types;
+
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
+public class NetworkingTag {
+    public String objectName;
+    public Translation3d position;
+    public Rotation3d rotation;
+    public int id;
+
+    public NetworkingTag(String objectName, Translation3d position, Rotation3d rotation, int id) {
+        this.objectName = objectName;
+        this.position = position;
+        this.rotation = rotation;
+        this.id = id;
+    }
+}
