43ff1419ffae41bdc64cee16a4c06870fea75188
nekiwo <58634064+nekiwo@users.noreply.github.com>
2024-02-03 18:25:38 -0600

    arm pid control

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -18,24 +18,43 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
  * An over the bumper intake subsystem that can rotate to give the note to
- * either the
- * ShooterSubsystem or SlamDunkerSubsystem.
+ * either the ShooterSubsystem or SlamDunkerSubsystem.
  */
 public class IntakeSubsystem extends SubsystemBase {
-	private static final double ENCODER_OFFSET = -0.18064158; // Radians
-	private static final double OUT_ANGLE_THRESHOLD = 0; // Radians
+	private static final double ENCODER_OFFSET = -0.18064158;
+	private static final double OUT_ANGLE = 0 * DEG;
+	private static final double IN_ANGLE = 180 * DEG;
+	private static final double SLAM_DUNKER_ANGLE = 0;
+	private static final double ANGLE_THRSHOLD = 5 * DEG;
+	
+	private static final double INTAKE_VELOCITY = 0.5;
 
-	private boolean intakeState = false;
+	private static final double MAX_VELOCITY = 1.0; // Maximum arm velocity in radians per second
+
+	private PIDController armPID = new PIDController(1.0, 0.0, 0.0);
+
+	public static enum ArmState {
+		In,
+		Out,
+		SlamDunker,
+		Rotating
+	}
+
+	public static enum IntakeState {
+		NoteIn,
+		NoteOut,
+		Intaking
+	}
+
+	private boolean isIntaking = false;
+	private IntakeState intakeState = IntakeState.NoteOut;
 	private double currentPosition = 0;
 	private double targetPosition = 0;
 
 	private WPI_TalonFX leftRotationMotor = new WPI_TalonFX(3);
 	private WPI_TalonFX rightRotationMotor = new WPI_TalonFX(14);
 	private WPI_TalonSRX intakeMotor = new WPI_TalonSRX(34);
-
 	private DutyCycleEncoder rotationEncoder = new DutyCycleEncoder(1);
-
-	private PIDController pid = new PIDController(1.0, 0.0, 0.0);
 
 	public IntakeSubsystem() {
 		rightRotationMotor.follow(leftRotationMotor);
@@ -46,17 +65,31 @@ public class IntakeSubsystem extends SubsystemBase {
 		rotationEncoder.reset();
 	}
 
-	private void setRotationVelocity(double angle) {
-		leftRotationMotor.set(ControlMode.Velocity, angle / FALCON_TICKS / 10.0);
+	/**
+	 * Sets arm rotation velocity
+	 * 
+	 * @param velocity velocity in radians per second
+	 */
+	private void setRotationVelocity(double velocity) {
+		leftRotationMotor.set(ControlMode.Velocity, velocity / FALCON_TICKS / 10.0);
 	}
 
 	/**
-	 * Gets gravitational counter-balancing velocity (setting arm to this should keep it in place)
+	 * Sets the velocity of the intake wheels
 	 * 
-	 * @return Motor velocity in Talon ticks per 100ms
+	 * @param velocity Falcon ticks/100ms speed
 	 */
-	private int getGravityVelocity() {
-		return 0;
+	public void setIntakeVelocity(double velocity) {
+		intakeMotor.set(ControlMode.Velocity, velocity);
+	}
+
+	/**
+	 * Returns true if intake limit switch is activated
+	 * 
+	 * @return boolean state
+	 */
+	private boolean getIntakeLimitSwitch() {
+		return false;
 	}
 
 	/**
@@ -78,31 +111,60 @@ public class IntakeSubsystem extends SubsystemBase {
 	}
 
 	/**
-	 * Returns true if intake is out
+	 * Returns arm state enum.
+	 * Possible states: in, out, slam dunker, rotating
 	 * 
-	 * @return Boolean state
+	 * @return IntakeSubsystem.ArmState state
 	 */
-	public boolean getIntakeState() {
-		return intakeState;
+	public ArmState getArmState() {
+		if (Math.abs(getPosition() - IN_ANGLE) < ANGLE_THRSHOLD) {
+			return ArmState.In;
+		} else if (Math.abs(getPosition() - OUT_ANGLE) < ANGLE_THRSHOLD) {
+			return ArmState.Out;
+		} else if (Math.abs(getPosition() - SLAM_DUNKER_ANGLE) < ANGLE_THRSHOLD) {
+			return ArmState.SlamDunker;
+		}
+		return ArmState.Rotating;
 	}
 
 	/**
-	 * Sets the speed of the intake wheels
+	 * Returns intake state.
+	 * Possible states: NoteIn, NoteOut, Intaking
 	 * 
-	 * @param speed Percent speed
+	 * @return IntakeSubsystem.IntakeState
 	 */
-	public void setIntakeOutput(double speed) {
-		intakeMotor.set(ControlMode.PercentOutput, speed);
+	public IntakeState getIntakeState() {
+		return intakeState;
+	}
+	
+	/**
+	 * Intakes note until it hits the limit switch.
+	 * Asynchronous, so call getIntakeState.
+	 */
+	public void intakeNote() {
+		setIntakeVelocity(INTAKE_VELOCITY);
+		isIntaking = true;
 	}
 
 	@Override
 	public void periodic() {
 		currentPosition = -(rotationEncoder.getAbsolutePosition() - rotationEncoder.getPositionOffset()) * 2 * Math.PI + ENCODER_OFFSET;
-		// intakeState = getPosition() < OUT_ANGLE_THRESHOLD;
+		SmartDashboard.putNumber("Intake Encoder", currentPosition / DEG);
 
-		SmartDashboard.putNumber("encoderPos", currentPosition / DEG);
+		double newVelocity = armPID.calculate(currentPosition, targetPosition);
+		setRotationVelocity(Math.copySign(Math.min(Math.abs(newVelocity), MAX_VELOCITY), newVelocity));
 
-		// double newVelocity = pid.calculate(currentPosition, targetPosition);
-		// setRotationVelocity(Math.max(newVelocity, 0.5));
+		if (getIntakeLimitSwitch()) {
+			intakeState = IntakeState.NoteIn;
+			if (isIntaking) {
+				setIntakeVelocity(0.0);
+			}
+		} else {
+			if (isIntaking) {
+				intakeState = IntakeState.Intaking;
+			} else {
+				intakeState = IntakeState.NoteOut;
+			}
+		}
 	}
 }
