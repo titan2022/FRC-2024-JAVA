8d1d81ac68d666bdf32a5285b0f3cfd82bdc925e
Ivan Chen <ivanchen3k@gmail.com>
2024-02-15 18:18:35 -0600

    Rewrote deleted Translation command

--- src/main/java/frc/robot/commands/TranslationCommand.java
+++ src/main/java/frc/robot/commands/TranslationCommand.java
@@ -5,9 +5,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.TranslationalDrivebase;
+import frc.robot.utility.Utility;
 
 /** An example command that uses an example subsystem. */
 public class TranslationCommand extends Command {
@@ -15,16 +17,21 @@ public class TranslationCommand extends Command {
   private TranslationalDrivebase driveBase;
   private Translation2d velocity;
   private double time;
-  private double startTime;
   private double endTime;
 
-  public TranslationCommand(double x, double y, double speed, TranslationalDrivebase driveBase) {
+  public TranslationCommand(Translation2d position, double speed, TranslationalDrivebase driveBase) {
+    this.driveBase = driveBase;
+    //Gets speed in direction of displacement
+    velocity = Utility.scaleVector(position, speed);
+    time = position.getNorm() / speed;
 
+    addRequirements(driveBase);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    endTime = Timer.getFPGATimestamp() + time;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -54,6 +61,9 @@ public class TranslationCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    if (Timer.getFPGATimestamp() > endTime)
+      return true;
+    else
+      return false;
   }
 }
