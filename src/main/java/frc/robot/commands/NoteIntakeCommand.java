1fefa15f9882464b72063e30992cbefc3e803458
Ivan Chen <ivanchen3k@gmail.com>
2024-02-22 18:06:41 -0600

    Started writing intake command

--- src/main/java/frc/robot/commands/NoteIntakeCommand.java
+++ src/main/java/frc/robot/commands/NoteIntakeCommand.java
@@ -5,19 +5,31 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.TranslationalDrivebase;
+import frc.robot.utility.Localizer;
 
 /** An example command that uses an example subsystem. */
-public class NoteIntakeCommand extends Command {
+public class NoteIntakeCommand extends SequentialCommandGroup {
   @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+  private IntakeSubsystem intake;
+  private TranslationalDrivebase driveBase;
+  private Localizer localizer;
 
-  public NoteIntakeCommand(IntakeSubsystem intake) {
 
+  public NoteIntakeCommand(IntakeSubsystem intake, TranslationalDrivebase driveBase, Localizer localizer) {
+    this.intake = intake;
+    this.driveBase = driveBase;
+    this.localizer = localizer;
+
+    addRequirements(driveBase, intake);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    intake.intake();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -28,6 +40,7 @@ public class NoteIntakeCommand extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    intake.stop();
   }
 
   // Returns true when the command should end.
