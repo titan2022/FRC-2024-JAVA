81304208596ff023989417300b38d68e9d48ffac
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2024-02-23 18:22:50 -0600

    Linkage math

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -19,6 +19,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ShooterSubsystem extends SubsystemBase {
 	private static final double MAX_ANGLE = 0;
 	private static final double MIN_ANGLE = 0;
+	
+	// TODO: measure these in CAD
+	private static final double SHOOTER_LENGTH = 1.0; // this is from the shooter pivot to its linkage connection
+	private static final double LINKAGE_LONG_ARM_LENGTH = 1.0;
+	private static final double LINKAGE_SHORT_ARM_LENGTH = 1.0;
+	// the distance of the motor axis from the shooter pivot
+	private static final double LINKAGE_PIVOT_DX = 1.0;
+	private static final double LINKAGE_PIVOT_DY = 1.0;
 
 	private double linkageEncoderValue = 0;
 	private double targetRotation = 0;
@@ -57,12 +65,25 @@ public class ShooterSubsystem extends SubsystemBase {
 	}
 
 	/**
-	 * Sets target angle of the intake
+	 * Sets target angle of the shooter
 	 * 
-	 * @param angle in radians (zero is ground, positive is up)
+	 * @param angle in radians (zero is parallel to the ground, positive is up)
 	 */
 	public void setRotation(double angle) {
-		targetRotation = angle; // TODO
+		if(angle < MIN_ANGLE || angle > MAX_ANGLE) return;
+		// trust me, the math is right
+		// TODO: check my math
+		double shooter_x = SHOOTER_LENGTH * Math.cos(angle);
+		double shooter_y = SHOOTER_LENGTH * Math.sin(angle);
+		double dx = shooter_x - LINKAGE_PIVOT_DX;
+		double dy = shooter_y - LINKAGE_PIVOT_DY;
+		double d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+		double theta1 = Math.acos( // Law of Cosines, can also be negative if we want the linkage "inside"
+			(Math.pow(LINKAGE_LONG_ARM_LENGTH, 2) - Math.pow(d, 2) - Math.pow(LINKAGE_SHORT_ARM_LENGTH, 2)) / 
+			(-2 * d * LINKAGE_SHORT_ARM_LENGTH)
+		);
+		double theta2 = Math.atan2(dx, dy);
+		targetRotation = Math.PI / 2 - (theta1 + theta2); // TODO
 	}
 
 	public void shoot(double velocity) {
