6483db47eb8eb69ca049883a9476fe2f404c3306
Ivan Chen <ivanchen3k@gmail.com>
2024-03-08 17:46:06 -0600

    Rewrote elevator subsystem and move command to be binary

--- src/main/java/frc/robot/commands/MoveElevatorCommand.java
+++ src/main/java/frc/robot/commands/MoveElevatorCommand.java
@@ -7,17 +7,18 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.SlamDunkerSubsystem;;
 
 /** An example command that uses an example subsystem. */
 public class MoveElevatorCommand extends Command {
+    public static final double RAISE_SPEED = 0.5;
+    public static final double LOWER_SPEED = -0.5;
+
     @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
-    public static final DEAD_BAND = 0.1;
     public ElevatorSubsystem elevator;
-    public double height;
-    public MoveElevatorCommand(double height, ElevatorSubsystem elevator) {
+    public boolean upward;
+    public MoveElevatorCommand(boolean up, ElevatorSubsystem elevator) {
         this.elevator = elevator;
-        this.height = height;
+        this.upward = up;
         
         addRequirements(elevator);
     }
@@ -30,19 +31,22 @@ public class MoveElevatorCommand extends Command {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        elevator.setHeight(height);
+        if (upward)
+            elevator.raise(RAISE_SPEED);
+        else 
+            elevator.lower(LOWER_SPEED);
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        
+        elevator.hold();
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        if (Math.abs(elevator.getHeight() - height) < 0.1)
+        if (elevator.isStalling())
             return true;
         else 
             return false;
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -19,21 +19,25 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utility.Constants;
 
-public class ElevatorSubsystem extends SubsystemBase{
+public class ElevatorSubsystem extends SubsystemBase {
+    public static final double RAISE_SPEED = 0.5;
+    public static final double LOWER_SPEED = -0.5;
+    public static final double STALL_CURRENT_LIMIT = 12;
+    // public static final double INDEXER_SPEED = 0.5;
     // TODO: get constants
     private final WPI_TalonFX LEFT_SPOOL_MOTOR = new WPI_TalonFX(0);
     private final WPI_TalonFX RIGHT_SPOOL_MOTOR = new WPI_TalonFX(0);
     private static final WPI_TalonFX INDEXER = new WPI_TalonFX(0);
 
-    public static final double BOTTOM_HEIGHT = 0.0;
-    public static final double TOP_HEIGHT = 21.5 * IN / METERS;
-    public static final double SPOOL_RADIUS = 1 * IN / METERS;
-    public static final double GEAR_RATIO = 30;
-    public static final double TOP_HEIGHT_TICKS = TOP_HEIGHT / (2 * SPOOL_RADIUS * FALCON_TICKS);
+    // public static final double BOTTOM_HEIGHT = 0.0;
+    // public static final double TOP_HEIGHT = 21.5 * IN / METERS;
+    // public static final double SPOOL_RADIUS = 1 * IN / METERS;
+    // public static final double GEAR_RATIO = 30;
+    // public static final double TOP_HEIGHT_TICKS = TOP_HEIGHT / (2 * SPOOL_RADIUS * FALCON_TICKS);
 
 
-    public static final double GRAVITY_OFFSET = 0;
-    public static final double ROBOT_WINCH_OFFSET = 0;
+    // public static final double GRAVITY_OFFSET = 0;
+    // public static final double ROBOT_WINCH_OFFSET = 0;
     
     // private static final int BOTTOM_ENCODER_TICKS = ENCODER_OFFSET;
     // private static final long TOP_ENCODER_TICKS = Math.round(ENCODER_OFFSET + (TOP_HEIGHT - BOTTOM_HEIGHT) / (2 * Math.PI * SPOOL_RADIUS) * Constants.Unit.FALCON_TICKS);
@@ -43,31 +47,31 @@ public class ElevatorSubsystem extends SubsystemBase{
     //     RIGHT_SPOOL_MOTOR
     // };
 
-    public static TalonFXConfiguration getSpoolTalonConfig() {
-        TalonFXConfiguration talon = new TalonFXConfiguration();
-        // Add configs here: 
-        talon.slot0.kP = 0.0;
-        talon.slot0.kI = 0.0;
-        talon.slot0.kD = 0.0;
-        talon.slot0.kF = 0;
-        talon.slot0.integralZone = 75;
-        talon.slot0.allowableClosedloopError = 5;
-        talon.slot0.maxIntegralAccumulator = 5120;
-        return talon;
-	}
+    // public static TalonFXConfiguration getSpoolTalonConfig() {
+    //     TalonFXConfiguration talon = new TalonFXConfiguration();
+    //     // Add configs here: 
+    //     talon.slot0.kP = 0.0;
+    //     talon.slot0.kI = 0.0;
+    //     talon.slot0.kD = 0.0;
+    //     talon.slot0.kF = 0;
+    //     talon.slot0.integralZone = 75;
+    //     talon.slot0.allowableClosedloopError = 5;
+    //     talon.slot0.maxIntegralAccumulator = 5120;
+    //     return talon;
+	// }
 
-    public static TalonFXConfiguration getIndexerTalonConfig() {
-        TalonFXConfiguration talon = new TalonFXConfiguration();
-        // Add configs here: 
-        talon.slot0.kP = 0.0;
-        talon.slot0.kI = 0.0;
-        talon.slot0.kD = 0.0;
-        talon.slot0.kF = 0;
-        talon.slot0.integralZone = 75;
-        talon.slot0.allowableClosedloopError = 5;
-        talon.slot0.maxIntegralAccumulator = 5120;
-        return talon;
-	}
+    // public static TalonFXConfiguration getIndexerTalonConfig() {
+    //     TalonFXConfiguration talon = new TalonFXConfiguration();
+    //     // Add configs here: 
+    //     talon.slot0.kP = 0.0;
+    //     talon.slot0.kI = 0.0;
+    //     talon.slot0.kD = 0.0;
+    //     talon.slot0.kF = 0;
+    //     talon.slot0.integralZone = 75;
+    //     talon.slot0.allowableClosedloopError = 5;
+    //     talon.slot0.maxIntegralAccumulator = 5120;
+    //     return talon;
+	// }
 
 
     public ElevatorSubsystem(){
@@ -75,57 +79,76 @@ public class ElevatorSubsystem extends SubsystemBase{
     }
 
     public void config() {
-        LEFT_SPOOL_MOTOR.configAllSettings(getSpoolTalonConfig());
-        RIGHT_SPOOL_MOTOR.configAllSettings(getSpoolTalonConfig());
+        // LEFT_SPOOL_MOTOR.configAllSettings(getSpoolTalonConfig());
+        // RIGHT_SPOOL_MOTOR.configAllSettings(getSpoolTalonConfig());
 
         RIGHT_SPOOL_MOTOR.follow(LEFT_SPOOL_MOTOR);
         RIGHT_SPOOL_MOTOR.setInverted(true);
         RIGHT_SPOOL_MOTOR.setSensorPhase(true);
 
-        LEFT_SPOOL_MOTOR.configIntegratedSensorInitializationStrategy(SensorInitializationStrategy.BootToZero);
-        LEFT_SPOOL_MOTOR.configIntegratedSensorInitializationStrategy(SensorInitializationStrategy.BootToZero);
+        LEFT_SPOOL_MOTOR.setNeutralMode(NeutralMode.Brake);
+        RIGHT_SPOOL_MOTOR.setNeutralMode(NeutralMode.Brake);
 
         INDEXER.configAllSettings(getIndexerTalonConfig());
         INDEXER.configIntegratedSensorInitializationStrategy(SensorInitializationStrategy.BootToZero);
     }
 
 
-    public void setHeight(double height) {
-        //Two stage elevator 
-        double motorUnits = height;
-        //Number of radians
-        motorUnits /= (SPOOL_RADIUS * GEAR_RATIO);
-        //Convert to number of FalconTicks
-        motorUnits /= FALCON_TICKS;
+    // public void setHeight(double height) {
+    //     //Two stage elevator 
+    //     double motorUnits = height;
+    //     //Number of radians
+    //     motorUnits /= (SPOOL_RADIUS * GEAR_RATIO);
+    //     //Convert to number of FalconTicks
+    //     motorUnits /= FALCON_TICKS;
 
-        LEFT_SPOOL_MOTOR.set(ControlMode.Position, motorUnits, DemandType.ArbitraryFeedForward, GRAVITY_OFFSET);
+    //     LEFT_SPOOL_MOTOR.set(ControlMode.Position, motorUnits, DemandType.ArbitraryFeedForward, GRAVITY_OFFSET);
+    // }
+
+    // public double getHeight() {
+    //     double position = LEFT_SPOOL_MOTOR.getSelectedSensorPosition();
+    //     if (position < 0) 
+    //          position = TOP_HEIGHT_TICKS + position;
+    //     position *= FALCON_TICKS;
+    //     position *= SPOOL_RADIUS * GEAR_RATIO;
+    //     return position;
+    // }
+
+    // public void winch(double climbDistance) {
+    //     double motorUnits = -climbDistance;
+    //     //Number of radians
+    //     motorUnits /= (SPOOL_RADIUS * GEAR_RATIO);
+    //     //Convert to number of FalconTicks
+    //     motorUnits /= FALCON_TICKS;
+    //     LEFT_SPOOL_MOTOR.set(ControlMode.Position, motorUnits, DemandType.ArbitraryFeedForward, -ROBOT_WINCH_OFFSET);
+    // }
+
+    public void raise() {
+        LEFT_SPOOL_MOTOR.set(ControlMode.PercentOutput, RAISE_SPEED);
     }
 
-    public double getHeight() {
-        double position = LEFT_SPOOL_MOTOR.getSelectedSensorPosition();
-        if (position < 0) 
-             position = TOP_HEIGHT_TICKS + position;
-        position *= FALCON_TICKS;
-        position *= SPOOL_RADIUS * GEAR_RATIO;
-        return position;
+    public void lower() {
+        LEFT_SPOOL_MOTOR.set(ControlMode.PercentOutput, LOWER_SPEED);
     }
 
-    public void winch(double climbDistance) {
-        double motorUnits = -climbDistance;
-        //Number of radians
-        motorUnits /= (SPOOL_RADIUS * GEAR_RATIO);
-        //Convert to number of FalconTicks
-        motorUnits /= FALCON_TICKS;
-        LEFT_SPOOL_MOTOR.set(ControlMode.Position, motorUnits, DemandType.ArbitraryFeedForward, -ROBOT_WINCH_OFFSET);
+    public void hold() {
+        LEFT_SPOOL_MOTOR.set(ControlMode.Velocity, 0);
     }
 
-    public void index(double displacement) {
-        INDEXER.set(ControlMode.Position, INDEXER.getSelectedSensorPosition() + displacement);
+    public boolean isStalling() {
+        if (LEFT_SPOOL_MOTOR.getOutputCurrent() > STALL_CURRENT_LIMIT) 
+            return true;
+        else 
+            return false;
     }
 
-    public void indexSpeed(double speed) {
+    public void index(double speed) {
         INDEXER.set(ControlMode.PercentOutput, speed);
     }
+
+    // public void indexSpeed(double speed) {
+    //     INDEXER.set(ControlMode.PercentOutput, speed);
+    // }
 
     public boolean hasNote() {
         return false;
