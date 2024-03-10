f6600f7192b43d149871d0f0496900a7eb8648ce
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2024-03-09 22:04:09 -0600

    Lots of stuff
    Mainly just PID for the shooter

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,8 +1,15 @@
 package frc.robot.subsystems;
 
+import static frc.robot.utility.Constants.Unit.FALCON_TICKS;
+import static frc.robot.utility.Constants.Unit.IN;
+import static frc.robot.utility.Constants.Unit.METERS;
+
+import java.net.IDN;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 
@@ -19,51 +26,51 @@ public class ElevatorSubsystem extends SubsystemBase {
     public static final WPI_TalonFX RIGHT_SPOOL_MOTOR = new WPI_TalonFX(0);
     public static final WPI_TalonFX INDEXER = new WPI_TalonFX(0);
     public static final DigitalInput NOTE_SENSOR = new DigitalInput(0);
-    // public static boolean hasNote = false;
-    // public static double noteDuration = 0;
-    // public static final double BOTTOM_HEIGHT = 0.0;
-    // public static final double TOP_HEIGHT = 21.5 * IN / METERS;
-    // public static final double SPOOL_RADIUS = 1 * IN / METERS;
-    // public static final double GEAR_RATIO = 30;
-    // public static final double TOP_HEIGHT_TICKS = TOP_HEIGHT / (2 * SPOOL_RADIUS * FALCON_TICKS);
+    public static boolean hasNote = false;
+    public static double noteDuration = 0;
+    public static final double BOTTOM_HEIGHT = 0.0;
+    public static final double TOP_HEIGHT = 21.5 * IN / METERS;
+    public static final double SPOOL_RADIUS = 1 * IN / METERS;
+    public static final double GEAR_RATIO = 28;
+    public static final double DISTANCE_PER_TICK = SPOOL_RADIUS * 2 * Math.PI * FALCON_TICKS / GEAR_RATIO;
+    public static final double TOP_HEIGHT_TICKS = (int)(TOP_HEIGHT / (DISTANCE_PER_TICK));
 
-
-    // public static final double GRAVITY_OFFSET = 0;
-    // public static final double ROBOT_WINCH_OFFSET = 0;
+    public static final int ENCODER_OFFSET = 0;
+    public static final double ROBOT_WINCH_OFFSET = 0;
     
-    // private static final int BOTTOM_ENCODER_TICKS = ENCODER_OFFSET;
-    // private static final long TOP_ENCODER_TICKS = Math.round(ENCODER_OFFSET + (TOP_HEIGHT - BOTTOM_HEIGHT) / (2 * Math.PI * SPOOL_RADIUS) * Constants.Unit.FALCON_TICKS);
+    private static final int BOTTOM_ENCODER_TICKS = ENCODER_OFFSET;
+    private static final long TOP_ENCODER_TICKS = Math.round(ENCODER_OFFSET + (TOP_HEIGHT - BOTTOM_HEIGHT) / (2 * Math.PI * SPOOL_RADIUS) * FALCON_TICKS);
     
-    // private final WPI_TalonFX[] spool_motors = new WPI_TalonFX[] {
-    //     LEFT_SPOOL_MOTOR,
-    //     RIGHT_SPOOL_MOTOR
-    // };
+    private final WPI_TalonFX[] spool_motors = new WPI_TalonFX[] {
+        LEFT_SPOOL_MOTOR,
+        RIGHT_SPOOL_MOTOR
+    };
 
-    // public static TalonFXConfiguration getSpoolTalonConfig() {
-    //     TalonFXConfiguration talon = new TalonFXConfiguration();
-    //     // Add configs here: 
-    //     talon.slot0.kP = 0.0;
-    //     talon.slot0.kI = 0.0;
-    //     talon.slot0.kD = 0.0;
-    //     talon.slot0.kF = 0;
-    //     talon.slot0.integralZone = 75;
-    //     talon.slot0.allowableClosedloopError = 5;
-    //     talon.slot0.maxIntegralAccumulator = 5120;
-    //     return talon;
-	// }
+    public static TalonFXConfiguration getSpoolTalonConfig() {
+        TalonFXConfiguration talon = new TalonFXConfiguration();
+        // Add configs here: 
+        talon.slot0.kP = 0.0;
+        talon.slot0.kI = 0.0;
+        talon.slot0.kD = 0.0;
+        talon.slot0.kF = 0;
+        talon.slot0.integralZone = 75;
+        talon.slot0.allowableClosedloopError = 5;
+        talon.slot0.maxIntegralAccumulator = 5120;
+        return talon;
+	}
 
-    // public static TalonFXConfiguration getIndexerTalonConfig() {
-    //     TalonFXConfiguration talon = new TalonFXConfiguration();
-    //     // Add configs here: 
-    //     talon.slot0.kP = 0.0;
-    //     talon.slot0.kI = 0.0;
-    //     talon.slot0.kD = 0.0;
-    //     talon.slot0.kF = 0;
-    //     talon.slot0.integralZone = 75;
-    //     talon.slot0.allowableClosedloopError = 5;
-    //     talon.slot0.maxIntegralAccumulator = 5120;
-    //     return talon;
-	// }
+    public static TalonFXConfiguration getIndexerTalonConfig() {
+        TalonFXConfiguration talon = new TalonFXConfiguration();
+        // Add configs here: 
+        talon.slot0.kP = 0.0;
+        talon.slot0.kI = 0.0;
+        talon.slot0.kD = 0.0;
+        talon.slot0.kF = 0;
+        talon.slot0.integralZone = 75;
+        talon.slot0.allowableClosedloopError = 5;
+        talon.slot0.maxIntegralAccumulator = 5120;
+        return talon;
+	}
 
 
     public ElevatorSubsystem(){
@@ -71,8 +78,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     public void config() {
-        // LEFT_SPOOL_MOTOR.configAllSettings(getSpoolTalonConfig());
-        // RIGHT_SPOOL_MOTOR.configAllSettings(getSpoolTalonConfig());
+        LEFT_SPOOL_MOTOR.configAllSettings(getSpoolTalonConfig());
 
         RIGHT_SPOOL_MOTOR.follow(LEFT_SPOOL_MOTOR);
         RIGHT_SPOOL_MOTOR.setInverted(true);
@@ -85,63 +91,52 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
 
-    // public void setHeight(double height) {
-    //     //Two stage elevator 
-    //     double motorUnits = height;
-    //     //Number of radians
-    //     motorUnits /= (SPOOL_RADIUS * GEAR_RATIO);
-    //     //Convert to number of FalconTicks
-    //     motorUnits /= FALCON_TICKS;
+    public void setHeight(double height) {
+        //Two stage elevator 
+        double motorUnits = height;
+        //Number of radians
+        motorUnits /= (SPOOL_RADIUS * GEAR_RATIO);
+        //Convert to number of FalconTicks
+        motorUnits /= FALCON_TICKS;
 
-    //     LEFT_SPOOL_MOTOR.set(ControlMode.Position, motorUnits, DemandType.ArbitraryFeedForward, GRAVITY_OFFSET);
-    // }
-
-    // public double getHeight() {
-    //     double position = LEFT_SPOOL_MOTOR.getSelectedSensorPosition();
-    //     if (position < 0) 
-    //          position = TOP_HEIGHT_TICKS + position;
-    //     position *= FALCON_TICKS;
-    //     position *= SPOOL_RADIUS * GEAR_RATIO;
-    //     return position;
-    // }
-
-    // public void winch(double climbDistance) {
-    //     double motorUnits = -climbDistance;
-    //     //Number of radians
-    //     motorUnits /= (SPOOL_RADIUS * GEAR_RATIO);
-    //     //Convert to number of FalconTicks
-    //     motorUnits /= FALCON_TICKS;
-    //     LEFT_SPOOL_MOTOR.set(ControlMode.Position, motorUnits, DemandType.ArbitraryFeedForward, -ROBOT_WINCH_OFFSET);
-    // }
-
-    public void elevate(double speed) {
-        if (isStalling()) 
-            LEFT_SPOOL_MOTOR.set(ControlMode.PercentOutput, Math.copySign(speed / 2, speed));
-        else 
-            LEFT_SPOOL_MOTOR.set(ControlMode.PercentOutput, speed);
-
+        LEFT_SPOOL_MOTOR.set(ControlMode.Position, motorUnits);
     }
+
+    public double getHeight() {
+        double position = LEFT_SPOOL_MOTOR.getSelectedSensorPosition();
+        position -= ENCODER_OFFSET;
+        return position * DISTANCE_PER_TICK;
+    }
+
+    public void winch(double climbDistance) {
+        double motorUnits = -climbDistance;
+        //Number of radians
+        motorUnits /= (SPOOL_RADIUS * GEAR_RATIO);
+        //Convert to number of FalconTicks
+        motorUnits /= FALCON_TICKS;
+        LEFT_SPOOL_MOTOR.set(ControlMode.Position, motorUnits);
+    }
+
+    // public void raise() {
+    //     LEFT_SPOOL_MOTOR.set(ControlMode.PercentOutput, RAISE_SPEED);
+    // }
+
+    // public void lower() {
+    //     LEFT_SPOOL_MOTOR.set(ControlMode.PercentOutput, LOWER_SPEED);
+    // }
 
     public void hold() {
         LEFT_SPOOL_MOTOR.set(ControlMode.Velocity, 0);
     }
 
-    public boolean isStalling() {
-        if (Math.abs(LEFT_SPOOL_MOTOR.getOutputCurrent()) > STALL_CURRENT_LIMIT) 
-            return true;
-        else 
-            return false;
-    }
+    // public boolean isStalling() {
+    //     if (LEFT_SPOOL_MOTOR.getOutputCurrent() > STALL_CURRENT_LIMIT) 
+    //         return true;
+    //     else 
+    //         return false;
+    // }
 
-    public void index(double speed) {
-        INDEXER.set(ControlMode.PercentOutput, speed);
-    }
-
-    public void stopIndex() {
-        INDEXER.set(ControlMode.Velocity, 0);
-    }
-
-    // public void indexSpeed(double speed) {
+    // public void index(double speed) {
     //     INDEXER.set(ControlMode.PercentOutput, speed);
     // }
 
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,14 +4,18 @@
 
 package frc.robot.subsystems;
 
+import static frc.robot.utility.Constants.Unit.*;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /***
@@ -19,16 +23,17 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * and shoot them into the speaker
  */
 public class ShooterSubsystem extends SubsystemBase {
-	private static final double MAX_ANGLE = 0;
-	private static final double MIN_ANGLE = 0;
+	private static final double MAX_ANGLE = 65*DEG;
+	private static final double MIN_ANGLE = 15*DEG;
 	
 	// TODO: measure these in CAD
-	private static final double SHOOTER_LENGTH = 1.0; // this is from the shooter pivot to its linkage connection
-	private static final double LINKAGE_LONG_ARM_LENGTH = 1.0;
-	private static final double LINKAGE_SHORT_ARM_LENGTH = 1.0;
+	private static final double SHOOTER_LENGTH = 7.078305*IN; // this is from the shooter pivot to its linkage connection
+	private static final double LINKAGE_LONG_ARM_LENGTH = 6.375*IN;
+	private static final double LINKAGE_SHORT_ARM_LENGTH = 2.0*IN;
 	// the distance of the motor axis from the shooter pivot
-	private static final double LINKAGE_PIVOT_DX = 1.0;
-	private static final double LINKAGE_PIVOT_DY = 1.0;
+	private static final double LINKAGE_PIVOT_DX = 5.270846*IN;
+	private static final double LINKAGE_PIVOT_DY = -0.57172*IN;
+	private static final double ANGLE_OFFSET = 16.9*DEG;
 
 //   //kP, kI, kD, kF
 // 	public static final double[] shooterPID = {0, 0, 0, 0};
@@ -36,18 +41,22 @@ public class ShooterSubsystem extends SubsystemBase {
 // 	public static final double[] shooterFeedForwardParams = {0, 0, 0};
 
 	// SimpleMotorFeedforward shooterFeedForward = new SimpleMotorFeedforward(shooterFeedForwardParams[0], shooterFeedForwardParams[1], shooterFeedForwardParams[2]);
-    public static final PIDController rotationPID = new PIDController(0, 0, 0);
+    public static final PIDController rotationPID = new PIDController(0.1, 0, 0);
     
-	public static final WPI_TalonFX linkageMotor = new WPI_TalonFX(12);
-	public static final WPI_TalonFX topShooterMotor = new WPI_TalonFX(13);
-	public static final WPI_TalonFX bottomShooterMotor = new WPI_TalonFX(5);
-	public static final WPI_TalonFX indexerMotor = new WPI_TalonFX(21);
-	public static final DutyCycleEncoder linkageEncoder = new DutyCycleEncoder(0);
+	public WPI_TalonFX linkageMotor = new WPI_TalonFX(12, "CANivore");
+	private WPI_TalonFX topShooterMotor = new WPI_TalonFX(13, "CANivore");
+	private WPI_TalonFX bottomShooterMotor = new WPI_TalonFX(5,"CANivore");
+	public WPI_TalonFX indexerMotor = new WPI_TalonFX(21, "CANivore");
+	public static DutyCycleEncoder linkageEncoder = new DutyCycleEncoder(9);
 
 	// public static boolean INDEX_ON = false;
 
 	public ShooterSubsystem() {
     	config();
+	}
+
+	public void setLinkageMotor(double value){
+		linkageMotor.set(ControlMode.PercentOutput, value);
 	}
 
 	public void config() {
@@ -68,8 +77,8 @@ public class ShooterSubsystem extends SubsystemBase {
 	 * 
 	 * @return Angle in radians (zero is ground, positive is up)
 	 */
-	public Rotation2d getRotation() {
-		return Rotation2d.fromRadians(linkageEncoder.get() / (2 * Math.PI));
+	public double getRotation() {
+		return linkageEncoder.get()*2.0*Math.PI;
 	}
 
 	/**
@@ -77,27 +86,26 @@ public class ShooterSubsystem extends SubsystemBase {
 	 * 
 	 * @param angle in radians (zero is parallel to the ground, positive is up)
 	 */
-	public void setRotation(Rotation2d theta) {
-		double angle = theta.getRadians();
-		if(angle < MIN_ANGLE || angle > MAX_ANGLE) return;
+	public void setRotation(double angle) {
+		SmartDashboard.putNumber("counter2", SmartDashboard.getNumber("counter2", 0.0) + 1);    
+		SmartDashboard.putNumber("angle", angle);
+		if(angle < MIN_ANGLE || angle > MAX_ANGLE) {
+			return;
+		}
 		// trust me, the math is right
-		// TODO: check my math
+		angle += ANGLE_OFFSET;
 		double shooter_x = SHOOTER_LENGTH * Math.cos(angle);
 		double shooter_y = SHOOTER_LENGTH * Math.sin(angle);
-		double dx = shooter_x - LINKAGE_PIVOT_DX;
+		double dx = LINKAGE_PIVOT_DX - shooter_x;
 		double dy = shooter_y - LINKAGE_PIVOT_DY;
 		double d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-		// double theta1 = Math.acos( // Law of Cosines, can also be negative if we want the linkage "inside"
-		// 	(Math.pow(LINKAGE_LONG_ARM_LENGTH, 2) - Math.pow(d, 2) - Math.pow(LINKAGE_SHORT_ARM_LENGTH, 2)) / 
-		// 	(-2 * d * LINKAGE_SHORT_ARM_LENGTH)
-		// );
     	double theta1 = Math.acos( // Law of Cosines, can also be negative if we want the linkage "inside"
 			(-Math.pow(LINKAGE_LONG_ARM_LENGTH, 2) + Math.pow(d, 2) + Math.pow(LINKAGE_SHORT_ARM_LENGTH, 2)) / 
 			(2 * d * LINKAGE_SHORT_ARM_LENGTH)
 		);
 		double theta2 = Math.atan2(dx, dy);
         double angleFromY = theta1 - theta2;
-		double targetRotation = Math.PI / 2 - angleFromY; // TODO
+		double targetRotation = Math.PI - angleFromY;
         targetRotation %= 2 * Math.PI;
 
         if (targetRotation < -Math.PI / 2) {
@@ -105,16 +113,24 @@ public class ShooterSubsystem extends SubsystemBase {
         } else if (targetRotation > 3 * Math.PI / 2) {
             targetRotation -= 2 * Math.PI;
         }
-
-        linkageMotor.set(ControlMode.Velocity, rotationPID.calculate(getRotation().getRadians(), targetRotation));
+		double linkageMag = rotationPID.calculate(getRotation(), targetRotation);
+        double PID = Math.copySign(Math.min(Math.abs(linkageMag), 10 / FALCON_TICKS), linkageMag);
+		linkageMotor.set(ControlMode.Velocity, 3000);
+		SmartDashboard.putNumber("target", targetRotation * 180 / Math.PI);
+		SmartDashboard.putNumber("PID", PID);
 	}
 
-	public void holdAngle() {
-		linkageMotor.set(ControlMode.Velocity, 0);
-	}
+	private double current_velocity = 0.0;
 
 	public void shoot(double velocity) {
-		topShooterMotor.set(ControlMode.PercentOutput, velocity);
+		current_velocity = velocity;
+		topShooterMotor.set(ControlMode.Velocity, velocity);
+		bottomShooterMotor.set(ControlMode.Velocity, velocity);
+	}
+
+	private double scale = 1.0;
+	public double getShooterVelocity(){
+		return current_velocity*10*(4*IN)*scale;
 	}
 
 	public void index(double speed) {
