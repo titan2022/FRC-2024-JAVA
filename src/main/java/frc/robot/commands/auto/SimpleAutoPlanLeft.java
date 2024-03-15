42f515aaf2f0928ae017de4cedc6c81edf83e9b5
Ivan Chen <ivanchen3k@gmail.com>
2024-03-15 17:43:34 -0500

    Simple auto

--- src/main/java/frc/robot/commands/auto/SimpleAutoPlanLeft.java
+++ src/main/java/frc/robot/commands/auto/SimpleAutoPlanLeft.java
@@ -26,6 +26,7 @@ public class SimpleAutoPlanLeft extends SequentialCommandGroup {
     public static double SHOOT_SPEAKER_SPEED = 0.5;
     public static final double SPEAKER_HEIGHT = 2 * METERS;
     public static Rotation2d SHOOT_ANGLE = Rotation2d.fromDegrees(60);
+    public static double MOVE_SPEED = 1;
 
     public SimpleAutoPlanLeft(TranslationalDrivebase translational, RotationalDrivebase rotational, ShooterSubsystem shooter, IndexerSubsystem indexer, IntakeSubsystem intake, ElevatorSubsystem elevator, Localizer localizer) {
         double sign = 1;
@@ -38,8 +39,12 @@ public class SimpleAutoPlanLeft extends SequentialCommandGroup {
         
         addCommands(
             new RotateShooterCommand(SHOOT_ANGLE, shooter),
-            new WaitCommand(1.0),
-            new SimpleShootCommand(SHOOT_SPEAKER_SPEED, shooter, indexer)
+            // new WaitCommand(1.0),
+            new SimpleShootCommand(SHOOT_SPEAKER_SPEED, shooter, indexer),
+            new TranslationCommand(new Translation2d(0, 3), MOVE_SPEED, translational),
+            new RotationCommand(Rotation2d.fromDegrees(sign * 45), rotational, localizer),
+            new TranslationCommand(new Translation2d(0, 5), MOVE_SPEED, translational)
+
             // new TranslationCommand(new Translation2d(sign * 0, 0.5), 1, translational),
             // new RotationCommand(Rotation2d.fromDegrees(45).times(-sign), rotational, localizer),
             // new TranslationCommand(new Translation2d(sign * 0, 2), 1, translational)
