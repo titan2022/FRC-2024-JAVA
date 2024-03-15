12b6783820351d567689feca097a9497bc8a7487
ethanc8 <echaroenpitaks@gmail.com>
2024-03-15 12:21:02 -0500

    [WIP] Work on amp alignment

--- src/main/java/frc/robot/commands/align/AlignAMPCommand.java
+++ src/main/java/frc/robot/commands/align/AlignAMPCommand.java
@@ -17,22 +17,28 @@ import frc.robot.utility.Localizer;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class AlignAMPCommand extends Command {
-	@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-	public static final int BLUE_AMP_APRILTAG = 6;
-	public static final int RED_AMP_APRILTAG = 5;
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    public static final int BLUE_AMP_APRILTAG = 6;
+    public static final int RED_AMP_APRILTAG = 5;
 
 // public static final double TRANSLATIONAL_SPEED = 1;
     public static final double ROTATIONAL_SPEED = 25 * DEG;
-    public static final double ANGLE_DEADBAND = 5 * DEG;
-    public static final double SUBWOOFER_LENGTH = 0.91 * METERS;
     public RotationalDrivebase rotational;
+    public TranslationalDrivebase translational;
     public Localizer localizer;
     public Rotation2d aprilTagAngle;
+    public Translation2d distanceFromAprilTag;
+    public static final Rotation2d rotEpsilon = new Rotation2d(5 * DEG);
+    public static final double transEpsilon = 0.01 * METERS;
+    public boolean hasRotated;
+    public boolean hasTranslated;
 
-    public AlignAMPCommand(RotationalDrivebase rotationalDrive, Localizer localizer) {
+    public AlignAMPCommand(RotationalDrivebase rotationalDrive, TranslationalDrivebase translationalDrive, Localizer localizer) {
         this.rotational = rotationalDrive;
+        this.translational = translationalDrive;
         this.localizer = localizer;
         addRequirements(rotationalDrive);
+        addRequirements(translationalDrive);
     }
 
     // Called when the command is initially scheduled.
@@ -42,18 +48,27 @@ public class AlignAMPCommand extends Command {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        if (Constants.getColor() == Alliance.Blue) {
-            // distanceFromAprilTag = localizer.getTagPosition(BLUE_AMP_APRILTAG);
-            aprilTagAngle = localizer.getTagHeading(BLUE_AMP_APRILTAG);
-        } else {
-            // distanceFromAprilTag = localizer.getTagPosition(RED_AMP_APRILTAG);
-            aprilTagAngle = localizer.getTagHeading(RED_AMP_APRILTAG);
+        if(!hasRotated) { // First rotate
+            if (Constants.getColor() == Alliance.Blue) {
+                distanceFromAprilTag = localizer.getTagPosition(BLUE_AMP_APRILTAG);
+            } else {
+                distanceFromAprilTag = localizer.getTagPosition(RED_AMP_APRILTAG);
+            }
+            Rotation2d speed = new Rotation2d(Math.copySign(ROTATIONAL_SPEED, aprilTagAngle.getRadians()));
+            SmartDashboard.putNumber("AMP Align Omega", speed.getDegrees());
+            rotational.setRotationalVelocity(speed);
+            SmartDashboard.putBoolean("AMP Align End", false);
+        } else { // Then translate
+            if (Constants.getColor() == Alliance.Blue) {
+                distanceFromAprilTag = localizer.getTagPosition(BLUE_AMP_APRILTAG);
+            } else {
+                distanceFromAprilTag = localizer.getTagPosition(RED_AMP_APRILTAG);
+            }
+            Rotation2d speed = new Rotation2d(Math.copySign(ROTATIONAL_SPEED, aprilTagAngle.getRadians()));
+            SmartDashboard.putNumber("AMP Align Omega", speed.getDegrees());
+            rotational.setRotationalVelocity(speed);
+            SmartDashboard.putBoolean("AMP Align End", false);
         }
-        Rotation2d speed = new Rotation2d(Math.copySign(ROTATIONAL_SPEED, aprilTagAngle.getRadians()));
-        SmartDashboard.putNumber("AMP Align Omega", speed.getDegrees());
-        rotational.setRotationalVelocity(speed);
-        SmartDashboard.putBoolean("AMP Align End", false);
-
     }
 
     // Called once the command ends or is interrupted.
@@ -66,9 +81,14 @@ public class AlignAMPCommand extends Command {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        if (Math.abs(aprilTagAngle.getRadians()) < ANGLE_DEADBAND) 
-            return true;
-        else 
+        if((Math.abs(aprilTagAngle.getRadians()) < rotEpsilon.getRadians())) {
+            hasRotated = true;
+            if(Math.abs(distanceFromAprilTag.getNorm()) < transEpsilon) {
+                hasTranslated = true;
+                return true;
+            }
             return false;
+        }
+        return false;
     }
 }
