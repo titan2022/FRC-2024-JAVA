30b33688bd2ad8f8aefdf4535b0ab29110b7b969
ethanc8 <echaroenpitaks@gmail.com>
2024-11-21 16:43:08 -0600

    put PID into SmartDashboard

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -151,6 +151,7 @@ public class ShooterSubsystem extends SubsystemBase {
 		
 		double linkageMag = rotationPID.calculate(getRotation(), targetRotation);
         double PID = Math.copySign(Math.min(Math.abs(linkageMag), 40 / FALCON_TICKS), linkageMag);
+		SmartDashboard.putNumber("PID", PID);
 		double FF = ((PID < 0) ? -0.04 : 0.04);
 		linkageMotor.set(ControlMode.Velocity, PID,
 			DemandType.ArbitraryFeedForward, FF
